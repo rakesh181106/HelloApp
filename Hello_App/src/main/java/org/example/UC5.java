90092beaef1504ef6436b659986e17d6e46c8914
rakesh181106 <rm8359@srmist.edu.in>
2026-03-26 15:29:41 +0530

    Added UC5

---
+++ Hello_App/src/main/java/org/example/UC5.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class UC5 {
+        public static void main(String[] args) {
+
+            StringBuilder names = new StringBuilder();
+
+            if (args.length == 0) {
+                names.append("World");
+            } else {
+                boolean first = true;
+                for (String name : args) {
+                    if (!first) {
+                        names.append(", ");
+                    }
+                    names.append(name);
+                    first = false;
+                }
+            }
+
+            System.out.println("Hello, " + names + "!");
+        }
+    }
+
