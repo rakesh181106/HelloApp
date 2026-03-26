723b9c522e030954502bf37550a146a8b1b87c41
rakesh181106 <rm8359@srmist.edu.in>
2026-03-26 15:22:39 +0530

    Added UC3

---
+++ Hello_App/src/main/java/org/example/UC_3.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class UC_3 {
+    public static void main(String[] args) {
+        String name = (args.length > 0) ? args[0] : "World";
+        System.out.println("Hello, " + name + "!");
+    }
+}
